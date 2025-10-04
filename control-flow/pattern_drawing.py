d67c5e4778de267567895e6bb3c8ef3a0700533a
Andrew Mawondo <andymawondo@gmail.com>
2025-10-04 21:35:17 +0200

    Add files via upload

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+number=int (input("Enter the size of the pattern:" ))
+rows=number
+while rows>0:
+    for i in range(number):
+        print("*",end=" "),
+    print()
+    rows-=1
+      
+        
